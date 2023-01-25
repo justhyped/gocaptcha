aec177df3b0aaf5b78cd84e3c454986b5a096064
justhyped <justhyped@protonmail.com>
2023-01-25 13:08:01 +0100

    add comment to SleepContext

--- internal/sleep.go
+++ internal/sleep.go
@@ -5,6 +5,7 @@ import (
 	"time"
 )
 
+// SleepContext sleeps for the specified duration but returns early with an error if the context is cancelled.
 func SleepContext(ctx context.Context, d time.Duration) error {
 	select {
 	case <-ctx.Done():
